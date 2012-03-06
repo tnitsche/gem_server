124306177bc3fc9648d2897d8069c4c7fd001e4b
Thomas Nitsche <thomas.nitsche@gmail.com>
2012-03-06 22:19:03 +0100

    Resetting port to default gem server port

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,12 +1,5 @@
-# Minimal sample configuration file for Unicorn (not Rack) when used
-# with daemonization (unicorn -D) started in your working directory.
-#
-# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
-# documentation.
-# See also http://unicorn.bogomips.org/examples/unicorn.conf.rb for
-# a more verbose configuration using more features.
 
-listen 8787 # by default Unicorn listens on port 8080
+listen 8808 # default gem server port 
 worker_processes 2 # this should be >= nr_cpus
 pid "./tmp/pids/unicorn.pid"
 stderr_path "./log/unicorn.log"
